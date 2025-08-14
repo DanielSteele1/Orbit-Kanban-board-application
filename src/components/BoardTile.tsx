51b77bb86c8e7634452c127a4ca782fbece36d3d
Daniel Steele <Dsteele1906@gmail.com>
2025-08-14 18:24:00 +0100

    Users can now also re-arrange boards using dnd-kit

--- src/components/BoardTile.tsx
+++ src/components/BoardTile.tsx
@@ -5,17 +5,29 @@ import { useNavigate } from 'react-router-dom';
 import BoardDropdown from "./BoardDropdown.tsx"
 import { GrDrag } from 'react-icons/gr';
 
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+
 interface BoardProps {
     board: BoardType;
-    handleDeleteBoard: (boardId:number) => (void);
+    handleDeleteBoard: (boardId: number) => (void);
 }
 
-function BoardTile({board, handleDeleteBoard}:BoardProps) {
+function BoardTile({ board, handleDeleteBoard }: BoardProps) {
+
+    const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id: board.id });
+
+    const style = {
+
+        transform: CSS.Transform.toString(transform),
+        transition,
+        width: "inherit",
+    };
 
     const navigate = useNavigate();
 
     return (
-        <div className="board-tile" // this should bind each board tile to it's corrosponding board, using it's ID
+        <div className="board-tile" ref={setNodeRef} {...attributes} style={style} // this should bind each board tile to it's corrosponding board, using it's ID
             onClick={() => navigate(`/board/${board.id}`)}
         >
             <div className="board-info">
@@ -30,12 +42,12 @@ function BoardTile({board, handleDeleteBoard}:BoardProps) {
             </div>
 
             <div className="board-dropdown">
-                <BoardDropdown 
+                <BoardDropdown
 
-                boardId={board.id}
-                handleDeleteBoard={handleDeleteBoard} 
+                    boardId={board.id}
+                    handleDeleteBoard={handleDeleteBoard}
                 />
-                <span className="icon">
+                <span className="icon" {...listeners}>
                     <GrDrag style={{ display: 'flex', marginLeft: '10px', verticalAlign: 'middle', cursor: 'pointer' }}
                     />
                 </span>
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -5,6 +5,9 @@ import BoardTile from "./BoardTile";
 import type { BoardType } from '../types';
 import { FaPlus } from 'react-icons/fa';
 
+import { DndContext, closestCorners, type DragEndEvent } from "@dnd-kit/core";
+import { horizontalListSortingStrategy, SortableContext, arrayMove } from "@dnd-kit/sortable";
+
 function Main() {
 
     // save boards to local storage 
@@ -46,41 +49,62 @@ function Main() {
 
     };
 
+
+    function handleDragEnd(event: DragEndEvent) {
+
+        const { active, over } = event;
+
+        if (active.id !== over?.id && over) {
+            setBoards((boards) => {
+                const oldIndex = boards.findIndex((b) => b.id === active.id);
+                const newIndex = boards.findIndex((b) => b.id === over.id);
+
+                return arrayMove(boards, oldIndex, newIndex);
+            });
+        }
+    };
+
+
     return (
-        <div className="main">
+        <DndContext collisionDetection={closestCorners} onDragEnd={handleDragEnd}>
+            <div className="main">
 
-            <div className="taskbar">
+                <div className="taskbar">
 
-                <span className="intro">
+                    <span className="intro">
 
-                    <span className="App-title" id="gradient"> Welcome to _____ ! </span>
+                        <span className="App-title" id="gradient"> Welcome to _____ ! </span>
 
-                    <span className="App-desc">
-                        Phalanx helps you organize your projects and tasks with ease.
-                        Create multiple boards to separate different areas of your work or life.
-                        You can re-order each board element inside each board to maintain full control of your workflow.
-                        Inside each board, you can add columns to categorize tasks and track progress step-by-step.
+                        <span className="App-desc">
+                            Phalanx helps you organize your projects and tasks with ease.
+                            Create multiple boards to separate different areas of your work or life.
+                            You can re-order each board element inside each board to maintain full control of your workflow.
+                            Inside each board, you can add columns to categorize tasks and track progress step-by-step.
+                        </span>
+                        <span id="gradient" className="App-desc"> Click on the 'Add a Board' button to get started. </span>
+
                     </span>
-                    <span id="gradient" className="App-desc"> Click on the 'Add a Board' button to get started. </span>
 
-                </span>
+                    <button id="button-text"
+                        className="add-board"
+                        onClick={handleAddBoard}>
+                        <FaPlus style={{ fontSize: '20px', margin: '5px' }} /> Add a New Board </button>
 
-                <button id="button-text"
-                    className="add-board"
-                    onClick={handleAddBoard}>
-                    <FaPlus style={{ fontSize: '20px', margin: '5px' }} /> Add a New Board </button>
+                </div>
 
+                <SortableContext
+                    items={boards}
+                    strategy={horizontalListSortingStrategy}>
+                    <div className="board-grid">
+
+                        {boards.map(board => (
+                            <BoardTile key={board.id} board={board} handleDeleteBoard={handleDeleteBoard} />
+                        ))}
+
+                    </div>
+                </SortableContext>
             </div>
-
-            <div className="board-grid">
-
-                {boards.map(board => (
-                    <BoardTile key={board.id} board={board} handleDeleteBoard={handleDeleteBoard} />
-                ))}
-
-            </div>
-
-        </div>
+        </DndContext>
     );
 }
 
